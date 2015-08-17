affd05c06f959db7d80b1e20cc9d136257eaa2d5
Peter Moresi <peter.moresi@gmail.com>
2015-08-16 22:00:08 -0700

    v1.0.2

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 (function (global, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(["exports", "module"], factory);
-  } else if (typeof exports !== "undefined" && typeof module !== "undefined") {
-    factory(exports, module);
+  if (typeof define === 'function' && define.amd) {
+    define(['exports', 'module', 'formula-flatten'], factory);
+  } else if (typeof exports !== 'undefined' && typeof module !== 'undefined') {
+    factory(exports, module, require('formula-flatten'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, mod);
+    factory(mod.exports, mod, global.FLATTEN);
     global.XOR = mod.exports;
   }
-})(this, function (exports, module) {
-  "use strict";
+})(this, function (exports, module, _formulaFlatten) {
+  'use strict';
 
   module.exports = XOR;
 
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+  var _FLATTEN = _interopRequireDefault(_formulaFlatten);
+
   function XOR() {
-    var args = fn.flatten(arguments);
+    var args = (0, _FLATTEN['default'])(arguments);
     var result = 0;
     for (var i = 0; i < args.length; i++) {
       if (args[i]) {
